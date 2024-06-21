4560410882cbb642dfcbe6af1ada5fc14f6cebab
Ozan Catalorman <94858358+ozancatalorman@users.noreply.github.com>
2024-06-21 12:56:39 +0300

    Necessary comments added

--- llm_assistant_ollama/src/assistant_langchain.py
+++ llm_assistant_ollama/src/assistant_langchain.py
@@ -4,7 +4,7 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.pydantic_v1 import BaseModel, Field
 import index_tools
 
-
+# Turns the function into JSON schema format for ollama to use
 def func_to_tool(func):
     return {
         "name": func.__name__,
@@ -19,7 +19,6 @@ def func_to_tool(func):
     }
 
 
-# Change the function to the JSON schema for model to use
 weather_tool = func_to_tool(index_tools.get_current_weather)
 
 # In this demo we have only one tool
@@ -48,19 +47,22 @@ model = model.bind_tools(tools=tools)
 def chat(user_input):
     response = model.invoke(user_input)
     if response.tool_calls:
+        # If the model response has a function call, extract function name and arguments
         function_name = response.tool_calls[0]["name"]
         function_arguments = response.tool_calls[0]["args"]
         if function_name == "get_current_weather":
+            # Executing the function and using the output to generate a response for the user response
             result = index_tools.get_current_weather(**function_arguments)
             prompt = PromptTemplate.from_template("""User's question{user_input}
                             Give me an answer with including this information {information} but do not use any tool for this operation""")
             chain = prompt | model | StrOutputParser()
             final_response = chain.invoke({"user_input": user_input, "information": result})
-            #final_response = answer_with_tool(user_input, result)
-            #final_resonse = f"The current weather in {result["location"]} is {result["weather_condition"].lower()} with a temperature of {result["temperature"]} degree {result["unit"]}."
+            # It can be returned as a structured string by us instead of making the model generate a response based on the function output. As below:
+            # final_resonse = f"The current weather in {result["location"]} is {result["weather_condition"].lower()} with a temperature of {result["temperature"]} degree {result["unit"]}."
             return final_response
         return f"Unknown function: {function_name}"
     else:
+        # If no function call return the response
         return response.content
 
 
--- llm_assistant_ollama/src/assistant_openai_api.py
+++ llm_assistant_ollama/src/assistant_openai_api.py
@@ -5,6 +5,7 @@ import index_tools
 
 client = OpenAI(base_url='http://localhost:11434/v1', api_key='dolphin-llama3')
 
+# Converts the function into JSON Schema format for API to use
 def convert_to_openani_fuction(func):
     return {
         "name": func.__name__,
@@ -23,7 +24,7 @@ functions = [
     convert_to_openani_fuction(index_tools.get_current_weather),
 ]
 
-
+# Parses the input str to find and extract the function call
 def parse_function_call(input_str):
     match = re.search(r'<functioncall>(.*?)</functioncall>', input_str, re.DOTALL)
     if match:
@@ -33,8 +34,9 @@ def parse_function_call(input_str):
             return None
     return None
 
-
+# Sends the messages to the model with API for generating a response
 def chat(messages):
+    # System prompt for the assistant to handle the function call
     system_message = f"""
     You are an AI Assistant that is an expert in following instructions and your name is ReefAssistant1.0. You will engage in conversation and provide information to the best of your knowledge. You have a set of functions you can use but only use if needed.
     You have access to these functions:
@@ -48,29 +50,36 @@ def chat(messages):
 
     Example: Tell the user how is the weather in the location user provided
 """
-
+    # Inserting the system message into messages and passing them to the model
     messages.insert(0, {"role": "system", "content": system_message})
     response = client.chat.completions.create(model="dolphin-llama3", messages=messages, functions=functions,
                                               temperature=0)
+    # Taking the content of the response and parsing it to find a function call if there is any
     message_content = response.choices[0].message.content
     function_call = parse_function_call(message_content)
     if function_call:
+        # If there is a function call, extract function name and arguments
         function_name = function_call["name"]
         function_arguments = function_call["arguments"]
         if function_name == "get_current_weather":
+            # Execute the function and push back the output to the model for it to generate a final response for the user
             data = index_tools.get_current_weather(**function_arguments)
             result = f"Relevant content:\n{data}\n\nUser arguments: {function_arguments}"
             content = chat([{"role": "user", "content": result}])
+            # Adding the system message to the conversation history
             conversation_history.append({"role": "system", "content": content})
             return content
         else:
             return f"Unknown function: {function_name}"
+    # If no function call return the response
     return message_content
 
 
 conversation_history = []
+# Loop for interacting with the model
 while True:
     user_input = input("User: ")
+    # Only using the last message in the conversation history
     conversation_history = conversation_history[-1:]
     conversation_history.append({"role": "user", "content": user_input})
     response = chat(conversation_history)
